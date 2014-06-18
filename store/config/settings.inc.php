aaa71f710829be0dcc783341d0b21dc52ee9b699
Jerome Guilloux <jguilloux@genymobile.com>
2014-06-18 13:37:56 +0200

    Bon utilisateur pour la base de données

--- store/config/settings.inc.php
+++ store/config/settings.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('_DB_SERVER_', 'localhost');
 define('_DB_NAME_', 'sonolocf_sonoloc71_db');
-define('_DB_USER_', 'sonoloc');
+define('_DB_USER_', 'sonolocf_sonoloc');
 define('_DB_PASSWD_', 'FmsP75.dsT9.');
 define('_DB_PREFIX_', 'ps_sonoloc71_');
 define('_MYSQL_ENGINE_', 'InnoDB');
