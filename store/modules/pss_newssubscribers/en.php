94b618906257b61dc99612d1b65b10eee2c53244
Jerome Guilloux <jguilloux@genymobile.com>
2014-07-10 13:21:48 +0200

    Mise à jour des images depuis le site web

---
+++ store/modules/pss_newssubscribers/en.php
@@ -0,0 +1,5 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{pss_slideshowpro}prestashop>pss_slideshowpro_160b153485f420fe11c47b8fc39c1727'] = 'http://www.prestascope.com/lang-en/content/10-module-slide-show-pro-installation';
