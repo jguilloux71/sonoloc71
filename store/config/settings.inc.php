debc67a00d6e7c71d1aa99481a9e6afe835c3e55
Jerome Guilloux <jerome.guilloux@stormshield.eu>
2020-01-19 09:56:09 +0100

    [Upgrade 1.6.1.24] Set version 1.6.1.24

--- store/config/settings.inc.php
+++ store/config/settings.inc.php
@@ -7,12 +7,11 @@ define('_DB_PREFIX_', 'ps_sonoloc71_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
 define('_PS_CACHE_ENABLED_', '0');
-define('_MEDIA_SERVER_1_', '');
-define('_MEDIA_SERVER_2_', '');
-define('_MEDIA_SERVER_3_', '');
+define('_PS_DIRECTORY_', '/install/upgrade/');
 define('_COOKIE_KEY_', 'tiIqFRNtpeEDOBPIiT5quOH3nMoRrGtTOfFRBy9h6BaiVhdCh8JSbY86');
 define('_COOKIE_IV_', 'eWCyRu7k');
-define('_PS_CREATION_DATE_', '2019-05-02');
-define('_PS_VERSION_', '1.6.1.24');
+define('_PS_CREATION_DATE_', '2014-02-14');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.24');
 define('_RIJNDAEL_KEY_', 'ZaK5V2XCTCyfS8ER2WvbXmpoa6x2VaKu');
 define('_RIJNDAEL_IV_', 'L1nJfaV/BSSQonM4ldkevg==');
